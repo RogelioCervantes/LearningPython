2100a6df0cc6c2f0c2808e1e362448760335026f
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-29 17:15:18 -0600

    added function computer_guess

--- Guess_number/main.py
+++ Guess_number/main.py
@@ -12,4 +12,23 @@ def guess(x):
             print("Too high")
     print(f"You did it! It was {rand_num}!")
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could also be high because low = high
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?\n--> ").lower()
+
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+
+    print("Congrats! You coded your first AI!!!")
+
+computer_guess(1000)
