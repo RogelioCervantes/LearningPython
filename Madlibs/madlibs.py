2835faf5bcdc008a2985dece954bf32888155e34
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-29 16:18:55 -0600

    first commit

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,7 @@
+# string concatenation (aka how to put strings together)
+
+food = "hamburger"
+
+print("I would like a " + food)
+print("I would like a {}".format(food))
+print(f"I would like a {food}") # this one is the best (imo)
