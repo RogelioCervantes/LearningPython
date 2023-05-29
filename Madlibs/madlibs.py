a41629c5f13e5dd0068ef619ee5752cc6b676866
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-29 16:31:54 -0600

    added inputs

--- Madlibs/madlibs.py
+++ Madlibs/madlibs.py
@@ -1,7 +1,15 @@
 # string concatenation (aka how to put strings together)
 
-food = "hamburger"
+#food = "hamburger"
 
-print("I would like a " + food)
-print("I would like a {}".format(food))
-print(f"I would like a {food}") # this one is the best (imo)
+#print("I would like a " + food)
+#print("I would like a {}".format(food))
+#print(f"I would like a {food}") # this one is the best (imo)
+
+someWord = input("Type something: ")
+someWord2 = input("Type something again: ")
+
+madlib = f"This is what you typed first: {someWord}\
+    \nAnd this is what you typed next: {someWord2}"
+
+print(madlib)
