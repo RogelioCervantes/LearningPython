8128e9097f38ae7d343232cb821cfca334944415
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-30 14:39:35 -0600

    players done (sort of)

--- TicTacToe/player.py
+++ TicTacToe/player.py
@@ -1,6 +1,6 @@
 import math, random
 
-class player:
+class Player:
     def __init__(self, letter):
         # letter is X or O
         self.letter = letter
@@ -14,11 +14,25 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
     
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "\'s turn. Input move (0-9): ")
+            # checking if this is a correct value casting it to an integer
+            # if it's not, then it's invalid
+            # if that spot is not available, it's invalid too
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try agian.")
