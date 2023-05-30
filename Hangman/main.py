81d9244e0819adb88515f2ade3420384e088ac63
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-30 07:42:02 -0600

    project completed

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,6 +1,6 @@
 import random
 from words import words
-import string
+import string, os
 
 def getWord(w):
     word = random.choice(w) # choice is for lists
@@ -10,17 +10,22 @@ def getWord(w):
     return word.upper()
 
 def hangman():
+    os.system("cls")
     word = getWord(words)
     word_letters = set(word) # letters in the word
     alphabet = set(string.ascii_letters)
     used_letters = set() # what the user has guessed
+    the_hangman = ["  __\n  |\n  O", "\n /", "|", "\\", "\n /", " \\"]
+    #  __
+    #  |
+    #  O
+    # /|\
+    # / \
 
-    lives = 6
-
+    lives = 0
     
-
     # finishes the loop when there's no more letters to guess
-    while len(word_letters) > 0 and lives > 0:
+    while len(word_letters) > 0 and lives < 6:
         # Current word.
         # replaces the letters that haven't been guessed
         # with an underscore (W _ R D)
@@ -28,13 +33,14 @@ def hangman():
         print("\nCurrent word: ", " ".join(word_list))
 
         guessed_letter = input("Guess a letter: ").upper()
+        os.system("cls")
         if guessed_letter in alphabet - used_letters:
             used_letters.add(guessed_letter)
             if guessed_letter in word_letters:
                 word_letters.remove(guessed_letter)
                 print("Correct!")
             else:
-                lives -= 1
+                lives += 1
                 print("Incorrect.")
                 
         elif guessed_letter in used_letters: 
@@ -42,14 +48,17 @@ def hangman():
         else:
             print("Invalid character. Try again.")
         
+        for x in range(lives):
+            print(the_hangman[x], end="")
+
         # Letters used.
         # join() turns used_letters into a string separated by 
         # whatever is before it, in this case a blank space.
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print(f"Attempts left: {lives}\nYou have used these letters: ", " ".join(used_letters))
+        print("\n\nYou have used these letters: ", " ".join(used_letters))
     
     # After the loop.
-    if lives == 0:
+    if lives == 6:
         print(f"\nYou died. The word was {word}")
     else:
         print(f"\nYou made it! The word was {word}")
