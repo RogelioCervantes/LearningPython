cf77c5107e48d87be9558108e2a9f0e848359962
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-30 14:30:59 -0600

    board done

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -3,4 +3,17 @@ class TicTacToe:
         self.board = [' ' for _ in range(9)] # this is the 3x3 board
         self.current_winer = None # keeps track of the winner
 
-                
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print("| " + " | ".join(row) + " |")
+    
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2. It tells what number corresponds to what box
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print("| " + " | ".join(row) + " |")
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == " "]
+            
