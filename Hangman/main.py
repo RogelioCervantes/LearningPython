b7fa02cc7bb26064213cfce6c65710594a5bc80f
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-30 07:06:53 -0600

    project completed (sort of)

--- Hangman/main.py
+++ Hangman/main.py
@@ -15,32 +15,43 @@ def hangman():
     alphabet = set(string.ascii_letters)
     used_letters = set() # what the user has guessed
 
-    # getting user input
-    while len(word_letters) > 0:
-        # Letters used.
-        # join() turns used_letters into a string separated by 
-        # whatever is before it, in this case a blank space.
-        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print("You have used these letters: ", " ".join(used_letters))
+    lives = 6
 
+    
+
+    # finishes the loop when there's no more letters to guess
+    while len(word_letters) > 0 and lives > 0:
         # Current word.
         # replaces the letters that haven't been guessed
         # with an underscore (W _ R D)
         word_list = [letter if letter in used_letters else '_' for letter in word]
-        print("Current word: ", " ".join(word_list))
+        print("\nCurrent word: ", " ".join(word_list))
 
         guessed_letter = input("Guess a letter: ").upper()
         if guessed_letter in alphabet - used_letters:
             used_letters.add(guessed_letter)
             if guessed_letter in word_letters:
                 word_letters.remove(guessed_letter)
+                print("Correct!")
+            else:
+                lives -= 1
+                print("Incorrect.")
                 
         elif guessed_letter in used_letters: 
             print("Already used that character. Try again.")
         else:
             print("Invalid character. Try again.")
-    # finishes the loop when there's no more letters to guess
+        
+        # Letters used.
+        # join() turns used_letters into a string separated by 
+        # whatever is before it, in this case a blank space.
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print(f"Attempts left: {lives}\nYou have used these letters: ", " ".join(used_letters))
+    
+    # After the loop.
+    if lives == 0:
+        print(f"\nYou died. The word was {word}")
+    else:
+        print(f"\nYou made it! The word was {word}")
 
-user_input = input("Type something: ")
-print(user_input)
 hangman()
