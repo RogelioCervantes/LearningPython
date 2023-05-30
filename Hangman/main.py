89e099c5359627656664478b003f64a9fe1109ee
Rogelio Cervantes <132374025+RogelioCervantes@users.noreply.github.com>
2023-05-30 06:15:05 -0600

    TODO: loop the program

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+import string
 
 def getWord(words):
     word = random.choice(words) # choice is for lists
@@ -10,3 +11,19 @@ def getWord(words):
 
 def hangman():
     word = getWord(words)
+    words_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_letters)
+    used_letters = set() # what the user has guessed
+
+    # getting user input
+    user_letter = input("Guess a letter: ").upper
+    if user_letter in alphabet - used_letters:
+        used_letters.add(used_letters)
+        if user_letter in words_letters:
+            words_letters.remove(user_letter)
+            
+    elif user_letter in used_letters: print("Already used that character. Try again.")
+    else: print("Invalid character. Try again.")
+
+user_input = input("Type something: ")
+print(user_input)
